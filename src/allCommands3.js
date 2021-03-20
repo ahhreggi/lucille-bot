0600befe71fb33e6d1b1ad0df1ef3073aa3532b3
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 16:28:36 -0400

    Add path

--- src/allCommands3.js
+++ src/allCommands3.js
@@ -1,10 +1,11 @@
 // Dynamically fetch all files with the prefix "command_" from the commands directory and register the command into allCommands
 
 const fs = require("fs");
+const path = require('path').resolve(__dirname, './commands');
 
 const allCommands = {};
 
-const commandFiles = fs.readdirSync("./commands").filter(file => file.startsWith("command_"));
+const commandFiles = fs.readdirSync(path).filter(file => file.startsWith("command_"));
 
 for (const file of commandFiles) {
   console.log(file);
