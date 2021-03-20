28e1603bd48cc07a857e855bc8bc804d26418c02
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 16:32:51 -0400

    Add try catch to allCommands3

--- src/allCommands3.js
+++ src/allCommands3.js
@@ -1,24 +1,33 @@
-// Dynamically fetch all files with the prefix "command_" from the commands directory and register the command into allCommands
-
 const fs = require("fs");
-const path = require('path').resolve(__dirname, './commands');
+const path = require('path')
+const commandsDirectory = path.resolve(__dirname, './commands');
 
 const allCommands = {};
 
-const commandFiles = fs.readdirSync(path).filter(file => file.startsWith("command_"));
+// Dynamically fetch all files with the prefix "command_" from the commands directory and register the command into allCommands
+const commandFiles = fs.readdirSync(commandsDirectory).filter(file => file.startsWith("command_"));
 
 for (const file of commandFiles) {
-  console.log(file);
-  // const COMMAND = require(`./commands/${file}`);
 
-  // const name = COMMAND.getName();
+  const COMMAND = require(`./commands/${file}`);
 
-  // allCommands[name] = {
-  //   desc: COMMAND.getDesc(),
-  //   roles: COMMAND.getRoles(),
-  //   alias: COMMAND.getAlias(),
-  //   cmd: COMMAND.getFunc()
-  // };
+  try {
+
+    const name = COMMAND.getName();
+
+    allCommands[name] = {
+      desc: COMMAND.getDesc(),
+      roles: COMMAND.getRoles(),
+      alias: COMMAND.getAlias(),
+      cmd: COMMAND.getFunc()
+    };
+
+  } catch (err) {
+
+    console.log(`Failed to add command in file: ${file}`);
+    console.log(err);
+
+  }
 
 }
 
