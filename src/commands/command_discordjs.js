0115ea7755a2ffe237ef0352f28649e7b62da642
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 19:37:22 -0400

    Add discordjs command

---
+++ src/commands/command_discordjs.js
@@ -0,0 +1,22 @@
+const Command = require("../command");
+const { codeBlock, getSenderVars } = require("../utility");
+
+///////////////////////////////////////////////////////////////////
+
+const name = "discordjs";
+const desc = "list discordjs info";
+const roles = ["user"];
+const alias = ["djs"];
+
+const cmdFunction = (message) => {
+
+  const senderVars = getSenderVars(message);
+  message.channel.send(codeBlock(`[DISCORDJS INFO FOR ${senderVars.senderTag}]\n\n` + Object.keys(senderVars)
+    .map(key => `${key}: ${senderVars[key]}`)
+    .join("\n")));
+
+};
+
+//////////////////////////////////////////////////////////////////
+
+module.exports = new Command(name, desc, roles, alias, cmdFunction);
