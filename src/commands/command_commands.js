fadf99df62198f3be5da3b4e27244d1b61806438
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 19:52:11 -0400

    Update command commands

--- src/commands/command_commands.js
+++ src/commands/command_commands.js
@@ -1,5 +1,8 @@
 const Command = require("../command");
-const allCommands = require("../allCommands");
+
+const fs = require("fs");
+const path = require("path");
+const commandsDirectory = path.resolve(__dirname, "../commands");
 
 ///////////////////////////////////////////////////////////////////
 
@@ -9,6 +12,33 @@ const roles = ["user"];
 const alias = ["cmds"];
 
 const cmdFunction = (message) => {
+
+  const commandFiles = fs.readdirSync(commandsDirectory).filter(file => file.startsWith("command_"));
+
+  const allCommands = {};
+
+  for (const file of commandFiles) {
+
+    const COMMAND = require(`./commands/${file}`);
+
+    try {
+
+      const name = COMMAND.getName();
+
+      allCommands[name] = {
+        desc: COMMAND.getDesc(),
+        roles: COMMAND.getRoles(),
+        alias: COMMAND.getAlias()
+      };
+
+    } catch (err) {
+
+      console.log(`Failed to retrieve command: ${file}`);
+      console.log(err);
+
+    }
+
+  }
 
   message.channel.send("**Available commands:**\n" + Object.keys(allCommands)
     .map(c => {
