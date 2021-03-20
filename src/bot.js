b4617bf432adb425cf7e5300e76b51f846e27a4d
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 04:15:35 -0400

    why me

--- src/bot.js
+++ src/bot.js
@@ -84,7 +84,7 @@ app.use((req, res, next) => {
 
 app.post("/*", function (req, res) {
   console.log(req.body);
-  res.send("success");
+  res.send({code: "success"});
 });
 
 const listener = app.listen(process.env.PORT, function () {
