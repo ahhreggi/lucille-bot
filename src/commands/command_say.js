e9be5f7f0917e58e55cb574cf99626b03e428118
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 19:21:40 -0400

    Add say command

---
+++ src/commands/command_say.js
@@ -0,0 +1,18 @@
+const Command = require("../command");
+
+///////////////////////////////////////////////////////////////////
+
+const name = "say";
+const desc = "lucille see, lucille do";
+const roles = ["admin", "mod", "vip"];
+const alias = [];
+
+const cmdFunction = (message, args) => {
+
+  message.channel.send(args.join(" "));
+
+};
+
+//////////////////////////////////////////////////////////////////
+
+module.exports = new Command(name, desc, roles, alias, cmdFunction);
