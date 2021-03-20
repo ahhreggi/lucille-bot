a136e250f1dc0fa21e18621bb2a0860905092ed7
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 16:26:10 -0400

    Add allCommands3 test

--- src/allCommands3.js
+++ src/allCommands3.js
@@ -7,16 +7,17 @@ const allCommands = {};
 const commandFiles = fs.readdirSync("./commands").filter(file => file.startsWith("command_"));
 
 for (const file of commandFiles) {
-  const COMMAND = require(`./commands/${file}`);
+  console.log(file);
+  // const COMMAND = require(`./commands/${file}`);
 
-  const name = COMMAND.getName();
+  // const name = COMMAND.getName();
 
-  allCommands[name] = {
-    desc: COMMAND.getDesc(),
-    roles: COMMAND.getRoles(),
-    alias: COMMAND.getAlias(),
-    cmd: COMMAND.getFunc()
-  };
+  // allCommands[name] = {
+  //   desc: COMMAND.getDesc(),
+  //   roles: COMMAND.getRoles(),
+  //   alias: COMMAND.getAlias(),
+  //   cmd: COMMAND.getFunc()
+  // };
 
 }
 
