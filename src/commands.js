bbec6d7ba80c801e388c8287d2b9ed6e97277adc
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-20 14:59:45 -0400

    Add Command class

---
+++ src/commands.js
@@ -0,0 +1,22 @@
+const { codeBlock } = require("./commandUtils");
+
+class Command {
+  constructor(name, description, func, permittedRoles = ["user"], alias = []) {
+    this.name = name;
+    this.desc = description;
+    this.cmd = func;
+    this.roles = permittedRoles;
+    this.alias = alias;
+  }
+
+  use(message) {
+    this.func(message);
+  }
+
+  help(message) {
+    message.channel.send(codeBlock("help 123"));
+  }
+
+}
+
+module.exports = Command;
