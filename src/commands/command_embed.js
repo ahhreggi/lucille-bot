2f34f0e45315be186e619a9352fc9e25d717f77f
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 15:24:13 -0400

    Add more syntax to embed cmd

--- src/commands/command_embed.js
+++ src/commands/command_embed.js
@@ -15,30 +15,37 @@ const cmdFunction = (message, args) => {
 
   const sectionDelim = "|";
   const fieldDelim = "%";
-  const color = "#fce303";
-  const space = { name: "\u200B", value: "\u200B" };
+  const omit = "$";
+  const space = "\u200B";
 
-  const fields = [];
+  let color = "#fce303"; // yellow
+
+  let embed = new Discord.MessageEmbed().setColor(color);
 
   try {
     let cmdArgs = args.join(" ").split(sectionDelim);
+    let fieldNum = 1;
     for (const arg of cmdArgs) {
       const [title, body] = arg.split(fieldDelim);
-      if (title === "$" && body === "$") {
-        fields.push(space);
-      } else {
-        fields.push({ name: title, value: body });
+      if (title === omit && body === omit) {
+        embed = embed.addField(space, space);
+      } else if (title === omit && fieldNum === 1) {
+        embed = embed.setDescription(body);
+      } else if (title === omit) {
+        embed = embed.addField(space, body);
+      } else if (title === "color") {
+        color = body;
+      } else if (!title && !body) {
+        embed = embed.addField(title, body);
       }
+      fieldNum++;
     }
-    message.delete();
+    // message.delete();
   } catch (err) {
     return message.channel.send("hey are you trying to kill me?! <:ahhknife2:823269952240091177>");
   }
 
-  const embed = new Discord.MessageEmbed()
-    .setColor(color)
-    .addFields(...fields);
-
+  embed = embed.setColor(color);
   message.channel.send(embed);
 
   // title1%this is the first sentence|title2%this is the second sentence|$%$|title3%this is the third sentence
