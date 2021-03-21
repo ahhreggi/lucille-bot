6ebda4eafcfd7e3cac369f19a719bb8b106d2159
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 00:16:55 -0400

    Add Response class

---
+++ src/response.js
@@ -0,0 +1,21 @@
+class Response {
+
+  // data = message/response content
+  // action = "send" or "return"
+  // key = if action = "send", key is ignored and data is posted to the channel
+  //       if action = "return", the key value is used to determine what bot.js will do
+  constructor(data, action, key) {
+
+    this.data = data;
+    this.action = action;
+    this.key = key;
+
+    if (this.action !== "return") {
+      this.key = null;
+    }
+
+  }
+
+}
+
+module.exports = Response;
