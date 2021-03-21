0e2fcf9143ebd78b8c5d75d337d68c08579400bc
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 02:01:39 -0400

    Add prompts.js

---
+++ src/data/prompts.js
@@ -0,0 +1,108 @@
+module.exports = [
+  {
+    triggers: [
+      "good morning lucille",
+      "morning lucille",
+    ],
+    responses: [
+      "good morning %MEMBER%! :)",
+      "~*yawn*~ morning!",
+      "g'day mate",
+      "good morning! :)",
+      "GOOOOOOOOOOOOOOD morning!",
+      "morning :D",
+      "<:ahhGM:730258615268540537>"
+    ]
+  },
+  {
+    triggers: [
+      "good afternoon"
+    ],
+    responses: [
+      "good ***morning*** >:("
+    ]
+  },
+  {
+    triggers: [
+      "no u"
+    ],
+    responses: [
+      "no u %MEMBER%"
+    ]
+  },
+  {
+    triggers: [
+      "shut up lucille"
+    ],
+    responses: [
+      "no u",
+      "ok :<",
+      "wtf did u just say to me u little burrito",
+      "wow ok i see u"
+    ]
+  },
+  {
+    triggers: [
+      "wake up",
+      "wake up lucille"
+    ],
+    responses: [
+      "I'M AWAKE D:"
+    ]
+  },
+  {
+    triggers: [
+      "bye"
+    ],
+    responses: [
+      "later nerd",
+      "cya l8r allig8r",
+      "byeee",
+      "okay bye :("
+    ]
+  },
+  {
+    triggers: [
+      "thanks lucille"
+    ],
+    responses: [
+      "np"
+    ]
+  },
+  {
+    triggers: [
+      "i win"
+    ],
+    responses: [
+      "no u don't lol"
+    ]
+  },
+  {
+    triggers: [
+      "good night lucille"
+    ],
+    responses: [
+      "good night %MEMBER% :)",
+      "sweet dreams %MEMBER% :)",
+      "good night don't forget me pls ty :>",
+      "good night! :D",
+      "night!"
+    ]
+  },
+  {
+    triggers: [
+      "<:ahhpog:797912934549422110>"
+    ],
+    responses: [
+      "<:ahhpog:797912934549422110>"
+    ]
+  },
+  {
+    triggers: [
+      "<:boogie:701313374733991998>"
+    ],
+    responses: [
+      "<:boogie:701313374733991998>"
+    ]
+  }
+];
