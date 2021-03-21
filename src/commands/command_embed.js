1418bd325f5b7ee28ba02e19c422fa9b77e838e1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 18:46:05 -0400

    Refactor embed

--- src/commands/command_embed.js
+++ src/commands/command_embed.js
@@ -12,81 +12,98 @@ const alias = [];
 const cmdFunction = (message, args) => {
 
   const outerDelim = "$";
-  const innerDelim = "--";
+  const innerDelim = ":";
   const space = { name: "\u200B", value: "\u200B" };
 
-  // const options = {
-  //   color: "#fce303",
-  //   title: null,
-  //   desc: null
-  // }
+  let argString = args.join(" ");
+
+  // Case 0: no arguments are given => send error
+  if (!argString) {
+    return message.channel.send("embed cannot be empty");
+  }
 
   let embed = new Discord.MessageEmbed();
 
-  let length = 0;
+  // Case 1: a plain message is given => embed the entire argString as a description and send
+  if (!argString.startsWith(outerDelim)) {
+    embed.setDescription(argString);
+    return message.channel.send(embed);
+  }
 
-  try {
-    // Parse args to isolate options
-    let argOpts = args.join(" ").split(outerDelim).slice(1);
-    console.log("argOpts", argOpts);
-    // For each option
-    for (const opt of argOpts) {
-      // Parse option to isolate property and value
-      const options = opt.split(innerDelim);
-      console.log("options", options);
-      const property = options[0];
+  // Case 2: options are provided => must start with $
 
-      // "space" => add a spacer
-      if (options[0] === "space") {
-        embed = embed.addField(space, space);
-        length++;
+  let valid = 0;
 
-      } else if (options.length > 1) {
+  // Parse args to isolate options
+  let argOpts = args.join(" ").split(outerDelim).slice(1); // .slice(1) to remove '' at index 0
 
-        const value = options[1];
+  // Validate option syntax (when split on the inner delim, will have a length of > 1)
+  // Invalid syntax => send an error
+  for (const opt of argOpts) {
 
-        // "color--ff0000" => set the embed color
-        if (property === "color") {
-          const color = value.startsWith("#") ? value : `#${value}`;
-          embed = embed.setColor(color);
-          length++;
+    const opts = opt.trim().replace(innerDelim, "%~%").split("%~%"); // in case innerDelim (:) is elsewhere in the value
 
-          // "title--This is the title" => set the title
-        } else if (property === "title") {
-          const title = value;
-          embed = embed.setTitle(title);
-          length++;
+    // If the option is a single string and not a space, syntax is invalid
+    if (opts.length === 1 && opts[0] !== "space") {
+      message.channel.send(`invalid option syntax: ${opts[0]}`);
+      return;
 
-          // "desc--This is the description" => set the description
-        } else if (property === "desc") {
-          const desc = value;
-          embed = embed.setDescription(desc);
-          length++;
+      // If the option is a single string and is a space, add a spacer
+    } else if (opts.length === 1 && opts[0] === "space") {
+      embed = embed.addField(space);
 
-          // "The field title--The field description" => add a field
-        } else if (property && value) {
-          embed = embed.addField(property, value);
-          length++;
-        }
+      // If the option is a property-value pair...
+    } else if (opts.length === 2) {
+
+      // Isolate the property and value
+      const property = opts[0].trim();
+      const value = opts[1].trim();
+
+      // VALID PROPERTIES:
+
+      // "color: ff0000" => set the embed color
+      if (property.toLowerCase() === "color" && value) {
+        const color = value.startsWith("#") ? value : `#${value}`; // --> TODO: check if # is necessary
+        embed = embed.setColor(color);
+
+        // "title: This is the title" => set the title
+      } else if (property.toLowerCase() === "title" && value) {
+        const title = value;
+        embed = embed.setTitle(title);
+        valid++;
+
+        // "desc: This is the description" => set the description
+      } else if (property.toLowerCase() === "desc" && value) {
+        const desc = value;
+        embed = embed.setDescription(desc);
+        valid++;
+
+        // If the property is none of the above, use the opts as the name and value of a field instead
+        // "The field title: The field description" => add a field
+      } else if (property && value) {
+        embed = embed.addField(property, value);
+        valid++;
       }
-    }
 
-    try {
-      if (length) {
-        message.channel.send(embed);
-      } else {
-        message.channel.send("embed shouldn't be empty");
-      }
-    } catch (err) {
-      message.channel.send("invalid syntax");
+      // Shouldn't ever reach this but just incase
+    } else {
+      return message.channel.send("unhandled case: opts was somehow split into 3");
     }
-    // message.delete();
-    return message.channel.send("no error");
-
-  } catch (err) {
-    return message.channel.send("hey are you trying to kill me?! <:ahhknife2:823269952240091177>");
 
   }
+
+  // The embed is sent only if at least one valid text option was set
+  if (valid) {
+    try {
+      message.channel.send(embed);
+      // message.delete();
+    } catch (err) {
+      return message.channel.send("hey are you trying to kill me?! <:ahhknife2:823269952240091177>");
+    }
+  } else {
+    message.channel.send("embed cannot be empty");
+  }
+
 };
 
 //////////////////////////////////////////////////////////////////
