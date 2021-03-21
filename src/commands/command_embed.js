c5099782ca33d5889adfeb80c79ac2c99d24aa93
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 15:38:51 -0400

    Fix embed maybe

--- src/commands/command_embed.js
+++ src/commands/command_embed.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const Command = require("../models/command");
-// const Response = require("../models/response");
-// const { hasRole } = require("../utility");
-// const errors = require("./configs/errors");
+const { codeBlock } = require("../utility");
 
 ///////////////////////////////////////////////////////////////////
 
@@ -20,35 +18,36 @@ const cmdFunction = (message, args) => {
 
   let color = "#fce303"; // yellow
 
-  let embed = new Discord.MessageEmbed().setColor(color);
+  const fields = [];
 
   try {
     let cmdArgs = args.join(" ").split(sectionDelim);
-    let fieldNum = 1;
     for (const arg of cmdArgs) {
       const [title, body] = arg.split(fieldDelim);
-      if (title === omit && body === omit) {
-        embed = embed.addField(space, space);
-      } else if (title === omit && fieldNum === 1) {
-        embed = embed.setDescription(body);
-      } else if (title === omit) {
-        embed = embed.addField(space, body);
-      } else if (title === "color") {
+      if (title === "color" && body.startsWith("#")) {
         color = body;
+      } else if (title === omit && body === omit) {
+        fields.push({ name: space, value: space });
+      } else if (title === omit) {
+        fields.push({ name: space, value: body });
       } else if (!title && !body) {
-        embed = embed.addField(title, body);
+        fields.push({ name: title, value: body });
       }
-      fieldNum++;
     }
     // message.delete();
   } catch (err) {
     return message.channel.send("hey are you trying to kill me?! <:ahhknife2:823269952240091177>");
   }
 
-  embed = embed.setColor(color);
-  message.channel.send(embed);
-
-  // title1%this is the first sentence|title2%this is the second sentence|$%$|title3%this is the third sentence
+  if (!fields.length) {
+    const embed = new Discord.MessageEmbed()
+      .setColor(color)
+      .addFields(...fields);
+    message.channel.send(embed);
+  } else {
+    message.channel.send("incorrect syntax bro, here's an example:");
+    message.channel.send(codeBlock("!embed color%#339FFF|title1%this is the first sentence|$%$|title2%this is the second sentence"));
+  }
 
 };
 
