7cc42cc36f544b6eecec0d77f1a7f76631f9c62a
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-21 15:03:18 -0400

    Test message delete, add try catch

--- src/commands/command_embed.js
+++ src/commands/command_embed.js
@@ -18,15 +18,20 @@ const cmdFunction = (message, args) => {
   const color = "#fce303";
   const space = { name: "\u200B", value: "\u200B" };
 
-  let cmdArgs = args.join(" ").split(sectionDelim);
-  const fields = [];
-  for (const arg of cmdArgs) {
-    const [title, body] = arg.split(fieldDelim);
-    if (title === "$" && body === "$") {
-      fields.push(space);
-    } else {
-      fields.push({ name: title, value: body });
+  try {
+    let cmdArgs = args.join(" ").split(sectionDelim);
+    const fields = [];
+    for (const arg of cmdArgs) {
+      const [title, body] = arg.split(fieldDelim);
+      if (title === "$" && body === "$") {
+        fields.push(space);
+      } else {
+        fields.push({ name: title, value: body });
+      }
     }
+    message.delete();
+  } catch (err) {
+    return message.channel.send("hey are you trying to kill me?! <:ahhknife2:823269952240091177>")
   }
 
   const embed = new Discord.MessageEmbed()
