bc347960f14b6d97d4aef31b5a67d0f061cb3ad1
Maria Regina Sirilan <ahhreggi@gmail.com>
2021-03-22 03:26:35 -0400

    Test embed

--- src/commands/command_embed.js
+++ src/commands/command_embed.js
@@ -16,58 +16,36 @@ const cmdFunction = (message, args) => {
 
   const error = codeBlock(embedHelp());
   let delim = "\\";
-  let forceSimple = false;
   let color;
-
-  let argStrIndex = 0;
 
   // Command usage: !embed -S -R -B -Y -G -W -K embedString
   // If an option is provided, the embed will be simple.
-  // If a delim is provided, the embed can be either simple or formatted.
 
   const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black", "white", "gray"];
 
   const arg = args[0];
   if (arg === "-S") {
-    forceSimple = true;
-    argStrIndex = 1;
+    color = "gray";
   } else if (colors.includes(arg)) {
     color = arg;
     console.log("color selected:", color);
     console.log("args:", args);
-  } else if (arg === "-!") {
-    delim = "!";
-  } else if (arg === "-%") {
-    delim = "%";
-  } else if (arg === "-&") {
-    delim = "&";
-  } else if (arg === "-$") {
-    delim = "$";
   }
 
   let embedString;
   let embedMsg;
   // If a delim is provided, embedString starts at index 1 and must start with delim
 
-  if ((delim !== "\\" || color) && args.length === 1) {
+  if (color && args.length === 1) {
     return message.channel.send(error);
   }
 
-  if (delim !== "\\") {
-    embedString = args.slice(1).join(" ");
-    if (args[1] && !args[1].startsWith(delim)) {
-      return message.channel.send(codeBlock("Message must be formatted if using an optional property identifier."));
-    } else if (args[1]) {
-      console.log("-->", embedString, delim);
-      embedMsg = embed(embedString, delim, false);
-    }
-
-  } else if (color || forceSimple) {
+  if (color) {
     embedString = args.slice(1).join(" ");
     embedMsg = embed(embedString, delim, true, color);
   } else if (!color) {
-    embedString = args.slice(argStrIndex).join(" ");
-    embedMsg = embed(embedString, delim, forceSimple);
+    embedString = args.join(" ");
+    embedMsg = embed(embedString, delim);
   }
 
   if (embedMsg) {
